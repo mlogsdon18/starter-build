5bf4051bee0b794404e40a883c60ca1961724aaa
Megan Logsdon <megan@heysparkbox.com>
2023-05-26 14:39:29 -0400

    test: add cypress test for foundry

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,22 @@
 describe('Sparkbox homepage', () => {
   it('can submit newsletter signup', () => {
-    cy.visit('https://sparkbox.com');
+    cy.visit('https://sparkbox.sparkboxqa.com/');
 
     cy.get('#mce-EMAIL').type('example@gmail.com');
 
     cy.get('#mc-embedded-subscribe-form').submit();
+  });
+
+  it('finds foundry article and navigates to it', () => {
+    cy.visit('https://sparkbox.sparkboxqa.com/');
+
+    cy.contains('Foundry').click();
+
+    cy.get('input[placeholder="Search the Foundry"]').type('eleventy');
+
+    cy.get('[data-content="Search"]').click();
+
+    cy.get('.cmp-listing__category').should('have.text', 'eleventy');
+
   });
 });
